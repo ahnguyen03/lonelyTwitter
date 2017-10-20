8695bc18a238bc2ef29ac34a5d7c69425db16457
Alex Nguyen <ahnguyen@ualberta.ca>
2017-10-19 23:17:50 -0600

    Add files via upload
    Lab 7 exercise added a way to test if its working

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -2,25 +2,68 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
-import android.util.Log;
 import android.widget.EditText;
-import android.widget.ListView;
 
 import com.robotium.solo.Solo;
 
 import junit.framework.TestCase;
 
-/**
- * Created by wz on 14/09/15.
- */
 public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2<LonelyTwitterActivity> {
-
+    private Solo solo;
 
     public LonelyTwitterActivityTest() {
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
     }
 
+    /**
+     * Runs at the begining of the tests
+     * pretty much this function runs all the time whenver you run a test
+      * @throws Exception
+     */
+    public void setUp() throws Exception{
+        solo = new Solo(getInstrumentation(),getActivity());
+
+    }
 
 
+    public void testStart() throws Exception {
+        Activity activity = getActivity();
 
+
+    }
+
+    public void testTweet(){
+        solo.assertCurrentActivity("Wrong Activity",LonelyTwitterActivity.class);
+        solo.enterText ((EditText)solo.getView(R.id.body),"Test Tweet!");
+        solo.clickOnButton("Save");
+        solo.enterText ((EditText)solo.getView(R.id.body),"");
+
+        assertTrue(solo.waitForText("Test Tweet!"));
+
+        solo.clickOnButton("Clear");
+
+    }
+
+    public void testClicktweetList(){
+        solo.assertCurrentActivity("Wrong Activity",LonelyTwitterActivity.class);
+        solo.clickOnButton("Clear");
+
+        solo.enterText((EditText)solo.getView(R.id.body), "Test Tweet!");
+        solo.clickOnButton("Save");
+        solo.waitForText("Test Tweet!");
+
+        solo.clickInList(0);
+        solo.assertCurrentActivity("Wrong Activity",EditTweetActivity.class);
+        assertTrue(solo.waitForText("Test Tweet!"));
+
+
+    }
+
+    /**
+     * at the end of the test it will close the activity
+     * @throws Exception
+     */
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
 }
