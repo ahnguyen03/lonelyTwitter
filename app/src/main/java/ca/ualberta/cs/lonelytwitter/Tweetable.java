4f7f8c6e20ae10b8b8e08315adcb60e16579f12a
Alex Nguyen <ahnguyen@ualberta.ca>
2017-11-02 18:25:57 -0600

    Refactor #2 took out public from interface

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -3,9 +3,11 @@ package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
 
+//Refactor #2 removed the public String getMessage() and made it into String getMessage()
+// reason is that its redundant to have for interface methods 
 public interface Tweetable {
-    public String getMessage();
+    String getMessage();
 
-    public Date getDate();
+    Date getDate();
 
 }
